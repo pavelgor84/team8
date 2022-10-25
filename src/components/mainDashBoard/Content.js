f5fdcf7b6cecfdd7b5c53c1931ba0e0776f2d38f
pavelgor84 <pavelgor@gmail.com>
2022-10-25 21:33:48 +0400

    conditional

--- src/components/mainDashBoard/Content.js
+++ src/components/mainDashBoard/Content.js
@@ -9,35 +9,35 @@ export default function Content({ esg }) {
     return (
 
         <div className={StyleContent.content}>
-
-            <div className={StylePlate.mainPlate}>
-                <div className={StylePlate.weight}>
-                    {db[esg][0].weight}
-                </div>
-                <div className={StylePlate.index}>
-                    {db[esg][0].index}
-                </div>
-                <div className={StylePlate.yearBlock}>
-                    <div className={StylePlate.yearLast}>
-                        Рейтинг {db[esg][0].yearLast}
+            {esg &&
+                <div className={StylePlate.mainPlate}>
+                    <div className={StylePlate.weight}>
+                        {db[esg][0].weight}
                     </div>
-                    <div className={StylePlate.yearCurrent}>
-                        Рейтинг {db[esg][0].yearCurrent}
+                    <div className={StylePlate.index}>
+                        {db[esg][0].index}
+                    </div>
+                    <div className={StylePlate.yearBlock}>
+                        <div className={StylePlate.yearLast}>
+                            Рейтинг {db[esg][0].yearLast}
+                        </div>
+                        <div className={StylePlate.yearCurrent}>
+                            Рейтинг {db[esg][0].yearCurrent}
+                        </div>
+                    </div>
+                    <div className={StylePlate.valueBlock}>
+                        <div className={StylePlate.valueLast}>
+                            {db[esg][0].previousNum}
+                        </div>
+                        <div className={StylePlate.valueCurrent}>
+                            {db[esg][0].currentNum}
+                        </div>
+                    </div>
+                    <div className={StylePlate.result}>
+                        {db[esg][0].currentNum - db[esg][0].previousNum}
                     </div>
                 </div>
-                <div className={StylePlate.valueBlock}>
-                    <div className={StylePlate.valueLast}>
-                        {db[esg][0].previousNum}
-                    </div>
-                    <div className={StylePlate.valueCurrent}>
-                        {db[esg][0].currentNum}
-                    </div>
-                </div>
-                <div className={StylePlate.result}>
-                    {db[esg][0].currentNum - db[esg][0].previousNum}
-                </div>
-            </div>
-
+            }
         </div>
     )
 }
