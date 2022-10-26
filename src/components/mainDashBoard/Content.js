99049eab1b875839782e742c9042c5afefac8740
pavelgor84 <pavelgor@gmail.com>
2022-10-26 13:06:05 +0400

    plate design

--- src/components/mainDashBoard/Content.js
+++ src/components/mainDashBoard/Content.js
@@ -21,30 +21,32 @@ export default function Content({ esg }) {
 
         <div className={StyleContent.content}>
             {esg &&
-                <div className={StylePlate.mainPlate}>
-                    <div className={StylePlate.weight}>
-                        <img src={svgWeight} alt="Вес" />
-                        {db[esg][0].weight}
-                    </div>
-                    <div className={StylePlate.index}>
-                        {db[esg][0].index}
-                    </div>
-                    <div className={StylePlate.yearBlock}>
-                        <div className={StylePlate.yearLast}>
-                            Рейтинг {db[esg][0].yearLast}
+                <div className={StylePlate.plateContainer}>
+                    <div className={StylePlate.mainPlate}>
+                        <div className={StylePlate.weight}>
+                            <img src={svgWeight} alt="Вес" />
+                            {db[esg][0].weight}
                         </div>
-                        <div className={StylePlate.yearCurrent}>
-                            Рейтинг {db[esg][0].yearCurrent}
+                        <div className={StylePlate.index}>
+                            {db[esg][0].index}
                         </div>
-                    </div>
-                    <div className={StylePlate.valueBlock}>
-                        <div className={StylePlate.valueLast}>
-                            <img src={svgCircleRed} alt="Кружок" />
-                            {db[esg][0].previousNum}
+                        <div className={StylePlate.yearBlock}>
+                            <div className={StylePlate.yearLast}>
+                                Рейтинг {db[esg][0].yearLast}
+                            </div>
+                            <div className={StylePlate.yearCurrent}>
+                                Рейтинг {db[esg][0].yearCurrent}
+                            </div>
                         </div>
-                        <div className={StylePlate.valueCurrent}>
-                            <img src={svgCircleGreen} alt="Вес" />
-                            {db[esg][0].currentNum}
+                        <div className={StylePlate.valueBlock}>
+                            <div className={StylePlate.valueLast}>
+                                <img src={svgCircleRed} alt="Кружок" />
+                                {db[esg][0].previousNum}
+                            </div>
+                            <div className={StylePlate.valueCurrent}>
+                                <img src={svgCircleGreen} alt="Вес" />
+                                {db[esg][0].currentNum}
+                            </div>
                         </div>
                     </div>
                     <div className={StylePlate.result}>
