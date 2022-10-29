fa69da0080b704d4a394e4d547003bdc21f1a894
pavelgor84 <pavelgor@gmail.com>
2022-10-29 12:30:34 +0400

    mainPage

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,13 +1,42 @@
 import React from "react";
 import Styles from "./MainPage.module.css"
+import logo from "./icons/vector.svg"
 
 export default function MainPage() {
     return (
         <div className={Styles.container}>
             <div className={Styles.loginBox}>
+                <div className={Styles.loginArea}>
+                    <div className={Styles.logo}>
+                        <img src={logo} alt="Enter" />
+                    </div>
+                    <div className={Styles.title}>
+                        <h1>Вход</h1>
+                    </div>
+                    <div className={Styles.user}>
+                        <input placeholder="логин" />
+                    </div>
+                    <div className={Styles.password}>
+                        <input type="password" placeholder="пароль" />
+                    </div>
+                </div>
+                <div className={Styles.linksArea}>
+                    <div className={Styles.linkProvider}>
+                        <img className={Styles.linkLogo} src={logo} alt="" />
+                        <a href="#"> Владелец показателей</a>
+                    </div>
+                    <div className={Styles.linkManager}>
+                        <img className={Styles.linkLogo} src={logo} alt="Руководитель функц. направления" />
+                        <a href="#"> Руководитель функц. направления</a>
+                    </div>
+                    <div className={Styles.boss}>
+                        <img className={Styles.linkLogo} src={logo} alt="Директор департамента" />
+                        <a href="#"> Директор департамента</a>
+                    </div>
+                </div>
 
             </div>
 
-        </div>
+        </div >
     )
 }
