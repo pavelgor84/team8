606846ea6e7cd2ee8030940d68851e4fe42ca1bc
pavelgor84 <pavelgor@gmail.com>
2022-11-04 16:45:22 +0400

    user panel

--- src/components/manager/Manager.js
+++ src/components/manager/Manager.js
@@ -6,11 +6,16 @@ import db from "./db.json"
 
 export default function Manager() {
     const [selected, setSelected] = useState("")
+    console.log(selected)
+    const userList = db.data.map((item, index) =>
+        <User user={item.name} occupation={item.occupation} selected={selected} setSelected={setSelected} needRevise={item.needRevise} key={index} />
+    )
 
     return (
         <div className={Styles.container}>
             <div className={Styles.sidebar}>
-                <User />
+                {/* <User user={db.data[0]} selected={selected} setSelected={setSelected} /> */}
+                {userList}
             </div>
             <div className={Styles.userData}>
                 <UserContent />
--- src/components/manager/User.js
+++ src/components/manager/User.js
@@ -4,13 +4,16 @@ import StatDone from "./icons/Status/done.svg"
 import StatNew from "./icons/Status/new.svg"
 
 
-export default function Manager() {
-
+export default function Manager({ user, occupation, needRevise, selected, setSelected }) {
+    function handleClick() {
+        setSelected(user)
+    }
     return (
-        <div className={Styles.container}>
-            <h4> Name </h4>
+        <div className={selected === user ? Styles.selected : Styles.container} onClick={handleClick}>
+            <h4> {user} </h4>
             <div className={Styles.bottomArea}>
-                <span> Occuation</span> <img src={StatDone} alt="Добавлено" />
+                <div> {occupation}</div>
+                <div>{needRevise ? <img src={StatNew} alt="Нужно утвердить" /> : <img src={StatDone} alt="Добавлено" />}</div>
             </div>
         </div>
     )
